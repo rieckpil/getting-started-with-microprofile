5b2f080dd085c54d66dd779f14fb549a5a0bb548
Philip Riecks <mail@philipriecks.de>
2019-08-20 07:11:40 +0200

    add schema annotations

--- microprofile-open-api/src/main/java/de/rieckpil/blog/Book.java
+++ microprofile-open-api/src/main/java/de/rieckpil/blog/Book.java
@@ -1,9 +1,17 @@
 package de.rieckpil.blog;
 
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+
+@Schema(name = "Book", description = "POJO that represents a book.")
 public class Book {
 
+    @Schema(required = true, example = "MicroProfile")
     private String title;
+
+    @Schema(required = true, example = "Duke")
     private String author;
+
+    @Schema(required = true, readOnly = true, example = "1")
     private Long id;
 
     public Book() {

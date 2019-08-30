e915d9e394044859d21478e752ced66193c6110b
Philip Riecks <mail@philipriecks.de>
2019-08-30 06:52:07 +0200

    publish OpenTracing

--- microprofile-open-tracing/book-store/src/main/java/de/rieckpil/blog/ContainerLoggingRequestFilter.java
+++ microprofile-open-tracing/book-store/src/main/java/de/rieckpil/blog/ContainerLoggingRequestFilter.java
@@ -3,19 +3,16 @@ package de.rieckpil.blog;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.ext.Provider;
-import java.io.IOException;
 import java.time.LocalDateTime;
 
 @Provider
-@Logged
 public class ContainerLoggingRequestFilter implements ContainerRequestFilter {
 
     @Override
-    public void filter(ContainerRequestContext requestContext)  {
+    public void filter(ContainerRequestContext requestContext) {
         System.out.println("==============");
         System.out.println("Incoming request at: " + LocalDateTime.now());
         requestContext.getHeaders().forEach((k, v) -> System.out.println(k + ":" + v));
         System.out.println("==============");
     }
 }
-

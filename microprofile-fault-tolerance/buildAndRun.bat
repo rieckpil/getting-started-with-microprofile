614b7a04a8cf72136bbf462eed57161faf6d49da
Philip Riecks <philip.riecks@scout24.com>
2019-09-06 16:04:34 +0200

    fix typo and missing execution right

--- microprofile-fault-tolerance/buildAndRun.bat
+++ microprofile-fault-tolerance/buildAndRun.bat
@@ -1,5 +1,5 @@
 @echo off
 call mvn clean package
-call docker build -t de.rieckpil.blog/microprofile-fault-tolernace .
-call docker rm -f microprofile-fault-tolernace
-call docker run -d -p 9080:9080 -p 9443:9443 --name microprofile-fault-tolernace de.rieckpil.blog/microprofile-fault-tolernace
+call docker build -t de.rieckpil.blog/microprofile-fault-tolerance .
+call docker rm -f microprofile-fault-tolerance
+call docker run -d -p 9080:9080 -p 9443:9443 --name microprofile-fault-tolerance de.rieckpil.blog/microprofile-fault-tolerance
--- microprofile-fault-tolerance/buildAndRun.sh
+++ microprofile-fault-tolerance/buildAndRun.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-mvn clean package && docker build -t de.rieckpil.blog/microprofile-fault-tolernace .
-docker rm -f microprofile-fault-tolernace || true && docker run -d -p 9080:9080 -p 9443:9443 --name microprofile-fault-tolernace de.rieckpil.blog/microprofile-fault-tolernace
+mvn clean package && docker build -t de.rieckpil.blog/microprofile-fault-tolerance .
+docker rm -f microprofile-fault-tolerance || true && docker run -d -p 9080:9080 -p 9443:9443 --name microprofile-fault-tolerance de.rieckpil.blog/microprofile-fault-tolerance

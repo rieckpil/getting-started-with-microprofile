ffa3a45a09cc53189b5545edaff092e700059ac7
rieckpil <philip.riecks@scout24.com>
2020-03-26 21:41:44 +0100

    add example for SimplyTimed annotation of MP Metrics 2.3

--- microprofile-metrics/src/main/java/de/rieckpil/blog/BookResource.java
+++ microprofile-metrics/src/main/java/de/rieckpil/blog/BookResource.java
@@ -1,6 +1,7 @@
 package de.rieckpil.blog;
 
 import org.eclipse.microprofile.metrics.annotation.Metered;
+import org.eclipse.microprofile.metrics.annotation.SimplyTimed;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
@@ -9,22 +10,32 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.concurrent.ThreadLocalRandom;
 
 @RequestScoped
 @Path("books")
 public class BookResource {
 
-    @Inject
-    private BookCommentClient bookCommentClient;
+  @Inject
+  private BookCommentClient bookCommentClient;
 
-    @Inject
-    private BookRequestProcessor bookRequestProcessor;
+  @Inject
+  private BookRequestProcessor bookRequestProcessor;
 
-    @GET
-    @Metered(name = "getBookCommentForLatestBookRequest", tags = {"spec=JAX-RS", "level=REST"})
-    @Produces(MediaType.TEXT_PLAIN)
-    public Response getBookCommentForLatestBookRequest() {
-        String latestBookRequestId = bookRequestProcessor.getLatestBookRequestId();
-        return Response.ok(this.bookCommentClient.getBookCommentByBookId(latestBookRequestId)).build();
-    }
+  @GET
+  @Metered(name = "getBookCommentForLatestBookRequest", tags = {"spec=JAX-RS", "level=REST"})
+  @Produces(MediaType.TEXT_PLAIN)
+  public Response getBookCommentForLatestBookRequest() {
+    String latestBookRequestId = bookRequestProcessor.getLatestBookRequestId();
+    return Response.ok(this.bookCommentClient.getBookCommentByBookId(latestBookRequestId)).build();
+  }
+
+  @GET
+  @SimplyTimed
+  @Path("/microprofile")
+  @Produces(MediaType.TEXT_PLAIN)
+  public Response getBook() throws InterruptedException {
+    Thread.sleep(ThreadLocalRandom.current().nextLong(100, 1000));
+    return Response.ok("Getting Started With Eclipse MicroProfile").build();
+  }
 }
